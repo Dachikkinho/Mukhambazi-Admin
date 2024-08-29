064d7938395476f6437814a63551edd1d9ba4676
Dachikk <dachikk.official@gmail.com>
2024-08-29 22:28:47 +0400

    feat/admin/addArtist-section

--- app/(authorised)/addArtist/page.tsx
+++ app/(authorised)/addArtist/page.tsx
@@ -1,13 +1,8 @@
 'use client';
 
-import { useForm } from 'react-hook-form';
 import styles from './page.module.scss';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
 import { ArtistSelect } from '@/app/components/addArtistSubComponents/ArtistSelect';
-import { FormStatus } from '@/app/components/addArtistSubComponents/FormStatus';
-import { CreateAuthor } from '@/app/interfaces/createAuthor.interface';
 import router from 'next/router';
 import ArtistForm from '@/app/components/addArtistSubComponents/ArtistForm';
 
