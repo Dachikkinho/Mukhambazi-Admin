c3046dc3fd36574c1c80bf99925c77deff6bcc26
Dachikk <dachikk.official@gmail.com>
2024-09-12 20:41:11 +0400

    fix/eslint

--- app/components/Artists/ArtistCard/ArtistCard.tsx
+++ app/components/Artists/ArtistCard/ArtistCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './ArtistCard.module.scss';
 
 type Props = {
@@ -5,25 +6,31 @@ type Props = {
     name: string;
     plays: string;
     pfp: string;
+    id: number;
 };
 
-const ArtistCard = ({ bgColor, name, plays, pfp }: Props) => {
+const ArtistCard = ({ bgColor, name, plays, pfp, id }: Props) => {
     return (
-        <div
-            className={styles.card}
-            style={{
-                backgroundColor: bgColor,
-            }}
-        >
-            <div className={styles.container}>
-                <img
-                    src={pfp}
-                    alt=""
-                    className={styles.pfp}
-                    draggable={false}
-                />
-                <p className={styles.name}>{name}</p>
-                <p className={styles.plays}>{plays} Plays</p>
+        <div className={styles.wrap}>
+            <Link href={`/addArtist?id=${id}`} className={styles.editButton}>
+                <img src="/icons/edit.svg" alt="" />
+            </Link>
+            <div
+                className={styles.card}
+                style={{
+                    backgroundColor: bgColor,
+                }}
+            >
+                <div className={styles.container}>
+                    <img
+                        src={pfp}
+                        alt=""
+                        className={styles.pfp}
+                        draggable={false}
+                    />
+                    <p className={styles.name}>{name}</p>
+                    <p className={styles.plays}>{plays} Plays</p>
+                </div>
             </div>
         </div>
     );
